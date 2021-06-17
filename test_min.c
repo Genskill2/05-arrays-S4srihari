4b5c95f507a2a30c349e6cc6606e8f6959400716
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-17 22:33:33 +0530

    Update test_min.c

--- test_min.c
+++ test_min.c
@@ -4,14 +4,6 @@
 
 int min(int [], int);
 
-int min(int a[], int i){
- int m=a[0];
-  for (int j=0;j<i;j++){
-  if (m>a[j]){m=a[j];}
-  }
-return m;
-}
-
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
   assert (min(x, 7) == -3);
@@ -20,3 +12,10 @@ int main(void) {
   assert (min(y, 1) == 5);
   printf("Min: passed\n");
   }
+int min(int a[], int i){
+ int m=a[0];
+  for (int j=0;j<i;j++){
+  if (m>a[j]){m=a[j];}
+  }
+return m;
+}
