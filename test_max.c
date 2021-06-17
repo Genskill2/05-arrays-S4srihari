b13741a8d11c745e9d5eb6bbddad413be55e3d83
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-17 20:45:23 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -2,8 +2,14 @@
 #include <assert.h>
 
 
-int max(int [], int);
+int max(int a[], int);
 
+int max(int a[],int i){
+int m= a[0];
+  for (int j=0;j<i;j++){
+  if (m<a[j]){m=a[j]}
+  }
+return m;}
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
   assert (max(x, 7) == 10);
