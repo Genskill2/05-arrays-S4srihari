bd2beb469692f28c0cf6bce757f1ebc135d306ea
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-17 20:49:06 +0530

    Update test_min.c

--- test_min.c
+++ test_min.c
@@ -4,6 +4,14 @@
 
 int min(int [], int);
 
+int min(int a[], int i){
+ int m=a[0];
+  for (int j=0;j<i;j++){
+  if m>a[j]{m=a[j];}
+  }
+return m;
+}
+
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
   assert (min(x, 7) == -3);
