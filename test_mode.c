1760bca59a1f068d5a339ad0c8d5e65182d13571
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-17 22:35:22 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -3,6 +3,15 @@
 
 
 int mode(int [], int);
+
+int main(void) {
+  int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
+  assert ((mode(x, 12) == 4));
+
+  int y[] = {5};
+  assert (mode(y, 1) == 5);
+  printf("Mode: passed\n");
+  }
 int mode(int a[],int length){
     int value=0;
     int max_count=0;
@@ -18,11 +27,3 @@ int mode(int a[],int length){
     }
     return value;
 }
-int main(void) {
-  int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
-  assert ((mode(x, 12) == 4));
-
-  int y[] = {5};
-  assert (mode(y, 1) == 5);
-  printf("Mode: passed\n");
-  }
