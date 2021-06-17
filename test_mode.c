90c744d60e676ecfc574d871ea5acf1d0648a95e
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-17 22:26:35 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -3,7 +3,21 @@
 
 
 int mode(int [], int);
-
+int mode(int a[],int length){
+    int value=0;
+    int max_count=0;
+    for(int i=0;i<length;i++){
+          int count=0;
+          for(int j=0;j<length;j++){
+            if(a[j]==a[i]){count=count+1;}
+          }
+          if (max_count<count){
+            value=a[i];
+            max_count=count;
+          }
+    }
+    return value;
+}
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
   assert ((mode(x, 12) == 4));
