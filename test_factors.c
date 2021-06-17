d3685e02a2121fe0124ddb5f752d8eb735214720
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-17 22:28:51 +0530

    Update test_factors.c

--- test_factors.c
+++ test_factors.c
@@ -3,7 +3,39 @@
 
 
 int factors(int, int []);
+int isprime(int);
+int isprime(int n){
+  for (int i = 2; i<n; i++){
+    if(n%i==0){
+       return 0;
+    }
+    else{
+     return 1;
+     }
+  }
 
+}
+
+int factors(int x, int arr[]){
+ int cnt=0;
+   for( int i = 2; i < x; i++){
+      while( x % i == 0){
+                if(isprime(i)){
+                     arr[cnt] = i ;
+                     x = x/i ; 
+                     cnt++;
+                     }
+                          
+                            if (isprime(x)){
+                               arr[cnt] = x;
+                              
+                               
+                           }
+                }
+        
+     }
+   return cnt+1;
+ }
 int main(void) {
   int ret[100] = {0};
   int count = factors(180, ret);
