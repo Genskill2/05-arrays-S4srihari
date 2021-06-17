3384e66cd5eb1f35a3b0de6d8020c3dd4b874d46
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-17 22:34:50 +0530

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -4,14 +4,6 @@
 
 float average(int a[], int);
 
-float average(int a[], int i){int sum=0;float avg;
-for (int j=0;j<i;j++){
- sum =sum + a[j];
- }
-avg = sum/(float)i;
- return avg;                             
-}
-
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
   assert ((average(x, 7) - 4.7142) < 0.001);
@@ -20,3 +12,10 @@ int main(void) {
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
   }
+float average(int a[], int i){int sum=0;float avg;
+for (int j=0;j<i;j++){
+ sum =sum + a[j];
+ }
+avg = sum/(float)i;
+ return avg;                             
+}
