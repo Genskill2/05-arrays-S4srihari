45cbff6250f6b8b159c0dd2ce65918836764295d
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-18 21:15:17 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 
 
-int max(int a[], int);
+
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
@@ -12,9 +12,3 @@ int main(void) {
   assert (max(y, 1) == 5);
   printf("Max: passed\n");
   }
-int max(int a[],int i){
-int m= a[0];
-  for (int j=0;j<i;j++){
-  if (m<a[j]){m=a[j];}
-  }
-return m;}
