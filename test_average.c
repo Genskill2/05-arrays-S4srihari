177d75cd49b7c6071b593238728db6685364bd08
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-18 21:19:48 +0530

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -2,12 +2,10 @@
 #include <assert.h>
 
 
-
-
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
   assert ((average(x, 7) - 4.7142) < 0.001);
-
+  printf("average: passed\n");
   int y[] = {5};
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
