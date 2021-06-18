4cd746efba8a01e43925c379dee1ce47a687b64d
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-18 20:33:09 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -71,3 +71,19 @@ int factors(int x, int arr[]){
      }
    return cnt+1;
  }
+int max(int a[], int);
+
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert (max(x, 7) == 10);
+
+  int y[] = {5};
+  assert (max(y, 1) == 5);
+  printf("Max: passed\n");
+  }
+int max(int a[],int i){
+int m= a[0];
+  for (int j=0;j<i;j++){
+  if (m<a[j]){m=a[j];}
+  }
+return m;}
