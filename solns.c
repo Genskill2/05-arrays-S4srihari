7a93fae414178841326c7623b369b40e6033cccb
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-18 20:33:56 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -87,3 +87,20 @@ int m= a[0];
   if (m<a[j]){m=a[j];}
   }
 return m;}
+int min(int [], int);
+
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert (min(x, 7) == -3);
+
+  int y[] = {5};
+  assert (min(y, 1) == 5);
+  printf("Min: passed\n");
+  }
+int min(int a[], int i){
+ int m=a[0];
+  for (int j=0;j<i;j++){
+  if (m>a[j]){m=a[j];}
+  }
+return m;
+}
