adebdd8183126034212dde75c743c4586ccc2420
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-18 20:32:13 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -19,3 +19,55 @@ for (int j=0;j<i;j++){
 avg = sum/(float)i;
  return avg;                             
 }
+int factors(int, int []);
+int isprime(int);
+
+int main(void) {
+  int ret[100] = {0};
+  int count = factors(180, ret);
+  assert (count == 5);
+  assert (ret[0] == 2);
+  assert (ret[1] == 2);
+  assert (ret[2] == 3);
+  assert (ret[3] == 3);
+  assert (ret[4] == 5);
+
+
+  count = factors(143, ret);
+  assert (count == 2);
+  assert (ret[0] == 11);
+  assert (ret[1] == 13);
+  printf("Factors: passed\n");
+}
+int isprime(int n){
+  for (int i = 2; i<n; i++){
+    if(n%i==0){
+       return 0;
+    }
+    else{
+     return 1;
+     }
+  }
+
+}
+
+int factors(int x, int arr[]){
+ int cnt=0;
+   for( int i = 2; i < x; i++){
+      while( x % i == 0){
+                if(isprime(i)){
+                     arr[cnt] = i ;
+                     x = x/i ; 
+                     cnt++;
+                     }
+                          
+                            if (isprime(x)){
+                               arr[cnt] = x;
+                              
+                               
+                           }
+                }
+        
+     }
+   return cnt+1;
+ }
