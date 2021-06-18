e6271256a75146efb0b3efce6ae0a2bb562031af
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-18 21:14:50 +0530

    Update test_factors.c

--- test_factors.c
+++ test_factors.c
@@ -2,8 +2,6 @@
 #include <assert.h>
 
 
-int factors(int, int []);
-int isprime(int);
 
 int main(void) {
   int ret[100] = {0};
@@ -22,35 +20,3 @@ int main(void) {
   assert (ret[1] == 13);
   printf("Factors: passed\n");
 }
-int isprime(int n){
-  for (int i = 2; i<n; i++){
-    if(n%i==0){
-       return 0;
-    }
-    else{
-     return 1;
-     }
-  }
-
-}
-
-int factors(int x, int arr[]){
- int cnt=0;
-   for( int i = 2; i < x; i++){
-      while( x % i == 0){
-                if(isprime(i)){
-                     arr[cnt] = i ;
-                     x = x/i ; 
-                     cnt++;
-                     }
-                          
-                            if (isprime(x)){
-                               arr[cnt] = x;
-                              
-                               
-                           }
-                }
-        
-     }
-   return cnt+1;
- }
