72037c3bd03d7d1c1abdd714a5eedb42bff12ef4
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-18 21:16:17 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -2,8 +2,6 @@
 #include <assert.h>
 
 
-int mode(int [], int);
-
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
   assert ((mode(x, 12) == 4));
@@ -12,18 +10,4 @@ int main(void) {
   assert (mode(y, 1) == 5);
   printf("Mode: passed\n");
   }
-int mode(int a[],int length){
-    int value=0;
-    int max_count=0;
-    for(int i=0;i<length;i++){
-          int count=0;
-          for(int j=0;j<length;j++){
-            if(a[j]==a[i]){count=count+1;}
-          }
-          if (max_count<count){
-            value=a[i];
-            max_count=count;
-          }
-    }
-    return value;
-}
+
